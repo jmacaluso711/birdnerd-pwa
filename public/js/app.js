38f27974bee9bfe07a2f25c88616da35512c9647
John Macaluso <macaluso.john@gmail.com>
2018-01-07 15:35:56 -0500

    remove account stuff for now

--- public/js/app.js
+++ public/js/app.js
@@ -2,5 +2,5 @@ import '../sass/app.scss';
 import { $, $$ } from './modules/bling';
 import makeMap from './modules/map';
 import './modules/birdform';
-import './modules/account';
+// import './modules/account';
 makeMap( $('#map') );
