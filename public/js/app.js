be6a9bdb22ee27726b014eaa8fd730d5bd2e778d
John Macaluso <macaluso.john@gmail.com>
2018-01-07 12:37:24 -0500

    user auth started

--- public/js/app.js
+++ public/js/app.js
@@ -2,4 +2,5 @@ import '../sass/app.scss';
 import { $, $$ } from './modules/bling';
 import makeMap from './modules/map';
 import './modules/birdform';
+import './modules/account';
 makeMap( $('#map') );
